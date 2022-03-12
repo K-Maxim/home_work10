f8c1d9a3e63cfe52d4be5b62ec4ac9fedb35148c
K-Maxim <101186665+K-Maxim@users.noreply.github.com>
2022-03-12 11:09:00 +0300

    Добавили кандидатов в json формате
    Добавили данные каждого кандидата.
    Данные:
    1. id
    2. Имя
    3. Фото
    4. Позиция
    5. Возраст
    6. Навыки

---
+++ main.py
@@ -0,0 +1,91 @@
+import json
+
+from flask import Flask
+
+
+def uploading_candidates():
+    """
+    Функция импортирует список из 'candidates.json' и записывает его в переменную
+    :return: список словарей
+    """
+    with open('candidates.json', 'r', encoding='UTF-8') as file:
+        candidates_list = json.load(file)
+        return candidates_list
+
+
+candidates_list = uploading_candidates()
+
+
+def canditates_on_page():
+    """
+    Функция, которая выводит данные кандидатов
+    :return: строковые данные всех кандидатов
+    """
+    person_str = ''
+
+    for candidate in candidates_list:
+        person = f'id: {candidate["id"]}\n' \
+                 f'name: {candidate["name"]}\n' \
+                 f'picture: {candidate["picture"]}\n' \
+                 f'position: {candidate["position"]}\n' \
+                 f'gender: {candidate["gender"]}\n' \
+                 f'age: {candidate["age"]}\n' \
+                 f'skills: {candidate["skills"]}\n\n'
+        person_str += person
+    return person_str
+
+
+person_str = canditates_on_page()  # строковые данные записываю в переменную
+
+app = Flask(__name__)  # запускаю фласк
+
+
+@app.route('/')
+def home_page():
+    """
+    Функция, выводит список кандидатов на главную страницу сайта
+    :return: данные всех кандидатов
+    """
+    return f'<pre>{person_str}<pre>'
+
+
+@app.route('/candidate/<int:id>')
+def candidate(id):
+    """
+    Функция, которая выводит данные кандидата по их "id"
+    :param id: тип данной: int.
+    У каждого кандидата свой "id" (в нашем случае числа будут от 1 до 7, так как 7 кандидатов)
+    :return: данные определенного кандидата
+    """
+    person_profile = ''
+
+    for i in range(len(candidates_list)):
+        if id == candidates_list[i]["id"]:
+            person = f'<img src="{candidates_list[i]["picture"]}">\n' \
+                     f'Имя кандидата: {candidates_list[i]["name"]}\n' \
+                     f'Позиция кандидата: {candidates_list[i]["position"]}\n' \
+                     f'Навыки кандидата: {candidates_list[i]["skills"]}'
+            person_profile += person
+        elif id > len(candidates_list):
+            return f'<h1>Кандидат не найден<h1>'
+    return f'<pre>{person_profile}<pre>'
+
+
+@app.route('/skills/<skills>')
+def skills(skills):
+    """
+    Функция, которая выводит данные кандидата по их "skills"
+    :param skills: тип данной: srt.
+    :return: данные определенного кандидата по их "skills"
+    """
+    person_profile = ''
+    for i in range(len(candidates_list)):
+        if skills in candidates_list[i]["skills"]:
+            person = f'Имя кандидата: {candidates_list[i]["name"]}\n' \
+                     f'Позиция кандидата: {candidates_list[i]["position"]}\n' \
+                     f'Навыки кандидата: {candidates_list[i]["skills"]}\n\n'
+            person_profile += person
+    return f'<pre>{person_profile}<pre>'
+
+
+app.run()
